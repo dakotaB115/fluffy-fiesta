401aa228a165e457a6adff16a9b1791ab950b78a
Dakota Brown <dakotabrown@MacBook-Pro-25.local>
2018-09-08 20:25:25 -0500

    Not working multiple users login test

--- MultiTestLogin/main.swift
+++ MultiTestLogin/main.swift
@@ -8,5 +8,60 @@
 
 import Foundation
 
-print("Hello, World!")
+class user {
+    var username: String
+    var password: String
+    
+    init(username: String, password: String) {
+        self.username = username
+        self.password = password
+    }
+}
 
+var users: [user] = []
+
+func login() {
+    var currentUser = [String]()
+    print("Username")
+    let username = String(readLine()!)
+    print("Password")
+    let password = String(readLine()!)
+    
+    currentUser.append(username)
+    currentUser.append(password)
+    print(currentUser)
+    
+    //need it to do something like this
+    if users.contains(currentUser) {
+        print("login successful")
+    }
+}
+
+func newUser() {
+    print("Username")
+    let username = String(readLine()!)
+    print("Password")
+    let password = String(readLine()!)
+    
+    users.append(user(username: username, password: password))
+    
+    dump(users)
+    
+    start()
+}
+
+func start() {
+    print("Current user Y/N")
+    var yN = String(readLine()!.uppercased())
+    
+    if yN == "Y" {
+        login()
+    } else if yN == "N" {
+        newUser()
+    } else if yN != "Y" || yN != "N" {
+        print("Current user Y/N")
+        yN = String(readLine()!.uppercased())
+    }
+}
+
+start()
